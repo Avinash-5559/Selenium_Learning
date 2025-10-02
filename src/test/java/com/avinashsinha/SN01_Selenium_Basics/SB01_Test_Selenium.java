8da4e8dd80b0b26dd0ea9ccf75dd9154ea0b4763
HP <sinha.avinash906@gmail.com>
2025-10-02 13:36:13 +0530

    Selenium_Learning

--- src/test/java/com/avinashsinha/SN01_Selenium_Basics/SB01_Test_Selenium.java
+++ src/test/java/com/avinashsinha/SN01_Selenium_Basics/SB01_Test_Selenium.java
@@ -2,6 +2,7 @@ package com.avinashsinha.SN01_Selenium_Basics;
 
 import io.qameta.allure.Description;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -14,15 +15,27 @@ public class SB01_Test_Selenium {
         ChromeDriver driver = new ChromeDriver();
         driver.get("https:app.vwo.com");
         System.out.println(driver.getTitle());
+        driver.quit();
     }
 
     @Test(priority = 2)
-    @Description("TC#2 : Verify to open the VWO Login Page on Firefox Browser")
+    @Description("TC#2 : Verify to open the VWO Login Page on Edge Browser")
+    public void test_VerifyVWOLogin_Edge() {
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https:app.vwo.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    @Test(priority = 3)
+    @Description("TC#3 : Verify to open the VWO Login Page on Firefox Browser")
     public void test_VerifyVWOLogin_Firefox() {
 
         FirefoxDriver driver = new FirefoxDriver();
         driver.get("https:app.vwo.com");
         System.out.println(driver.getTitle());
+        driver.quit();
 
     }
 }
